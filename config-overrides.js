14fa025121481d5f1cf463ddbe3709b67bc3c3c9
mab <git@ma-b.com>
2018-02-23 16:42:04 +0100

    Provide a file for overriding less variables
    	Mainly to offer a way to customize antd theme.
    	For all available vars, see: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less

--- config-overrides.js
+++ config-overrides.js
@@ -8,13 +8,15 @@ const rewireLess   = require('react-app-rewire-less');
 const rewireSass   = require('react-app-rewire-sass-modules');
 const rewireImport = require('react-app-rewire-import');
 
+const lessOptions  = require('./less-overrides');
+
 module.exports = function override (config, env) {
   config = rewireEslint(config, env, options => {
     options.emitWarning = process.env.NODE_ENV !== 'production' || process.env.REACT_APP_FORCE_BUILD;
     return options;
   });
   config = rewireImport(config, env, { libraryName: 'antd', libraryDirectory: 'es', style: true });
-  config = rewireLess(config, env);
+  config = rewireLess.withLoaderOptions(lessOptions)(config, env);
   config = rewireSass(config, env);
 
   return config;
---
+++ less-overrides.js
@@ -0,0 +1,6 @@
+module.exports = {
+  modifyVars: {
+    '@custom-variable-1': 'custom value',
+    '@custom-variable-2': 'custom value',
+  },
+};
