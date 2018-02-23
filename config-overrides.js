18edc4e7e40ef8c0f59d91409cb78728b41e8ee3
mab <git@ma-b.com>
2018-02-23 17:27:19 +0100

    Reorganize config-overrides to better readability

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 /* eslint
   import/no-extraneous-dependencies: off,
+  import/newline-after-import: off,
   no-param-reassign: off,
 */
 
@@ -9,14 +10,16 @@ const rewireSass   = require('react-app-rewire-sass-modules');
 const rewireImport = require('react-app-rewire-import');
 const rewireRHL    = require('react-app-rewire-hot-loader');
 
-const lessOptions  = require('./less-overrides');
+const lessOptions   = require('./less-overrides');
+const importOptions = { libraryName: 'antd', libraryDirectory: 'es', style: true };
+const eslintOptions = options => {
+  options.emitWarning = process.env.NODE_ENV !== 'production' || process.env.REACT_APP_FORCE_BUILD;
+  return options;
+};
 
 module.exports = function override (config, env) {
-  config = rewireEslint(config, env, options => {
-    options.emitWarning = process.env.NODE_ENV !== 'production' || process.env.REACT_APP_FORCE_BUILD;
-    return options;
-  });
-  config = rewireImport(config, env, { libraryName: 'antd', libraryDirectory: 'es', style: true });
+  config = rewireEslint(config, env, eslintOptions);
+  config = rewireImport(config, env, importOptions);
   config = rewireLess.withLoaderOptions(lessOptions)(config, env);
   config = rewireSass(config, env);
   config = rewireRHL(config, env);
