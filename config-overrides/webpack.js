850af70b45191763890e859e7d38eca0e089269c
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-03-06 19:03:40 +0100

    Explain Webpack overrides through config-override

--- config-overrides/webpack.js
+++ config-overrides/webpack.js
@@ -37,15 +37,52 @@ const eslintOptions = options => {
 };
 
 module.exports = function overrideWebpack (config, env) {
+  /**
+   * Allow the use of `.eslintrc.js` file instead of rules bundled with create-react-app
+   * https://github.com/timarney/react-app-rewired/tree/master/packages/react-app-rewire-eslint
+   */
   config = rewireEslint(config, env, eslintOptions);
+
+  /**
+   * Enable short imports for Ant Design.
+   * https://github.com/ant-design/babel-plugin-import
+   */
   config = rewireImport(config, env, importOptions);
+
+  /**
+   * Add Less file loader & inject variables.
+   * https://github.com/timarney/react-app-rewired/tree/master/packages/react-app-rewire-less
+   */
   config = rewireLess.withLoaderOptions(lessOptions)(config, env);
+
+  /**
+   * Add SCSS files loader
+   * https://github.com/buzhanguo/react-app-rewire-sass-modules
+   */
   config = rewireSass(config, env);
+
+  /**
+   * Enable Hot Loader
+   * https://github.com/cdharris/react-app-rewire-hot-loader
+   */
   config = rewireRHL(config, env);
+
+  /**
+   * Paths to include in loader rules
+   * https://webpack.js.org/configuration/module/#rule-include
+   */
   config = rewireModulesIncludes(config, env, settings.modulesPaths);
 
+  /**
+   * Whether to resolve symlinks to their symlinked location.
+   * https://webpack.js.org/configuration/resolve/#resolve-symlinks
+   */
   config.resolve.symlinks = false;
 
+  /**
+   * Tell webpack what directories should be searched when resolving modules.
+   * https://webpack.js.org/configuration/resolve/#resolve-modules
+   */
   config.resolve.modules.unshift(...settings.modulesPaths);
 
   return config;
