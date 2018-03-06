adcc1012c1c9ca6f832b02849b1f1666a356911e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-03-06 17:26:23 +0100

    Extend Webpack includes dir for modules rules

--- config-overrides.js
+++ config-overrides.js
@@ -12,6 +12,22 @@ const rewireSass   = require('react-app-rewire-sass-modules');
 const rewireImport = require('react-app-rewire-import');
 const rewireRHL    = require('react-app-rewire-hot-loader');
 
+const rewireModulesIncludes = (config, env, paths) => {
+  const crawl = obj => {
+    obj.forEach(rule => {
+      if (rule.oneOf) {
+        crawl(rule.oneOf);
+      } else if (rule.include && rule.include === path.resolve(__dirname, 'src')) {
+        rule.include = paths;
+      }
+    });
+  };
+
+  crawl(config.module.rules);
+
+  return config;
+};
+
 const lessOptions   = require('./less-overrides');
 const importOptions = { libraryName: 'antd', libraryDirectory: 'es', style: true };
 const eslintOptions = options => {
@@ -30,6 +46,9 @@ const webpack = function overrideWebpack (config, env) {
   config = rewireLess.withLoaderOptions(lessOptions)(config, env);
   config = rewireSass(config, env);
   config = rewireRHL(config, env);
+  config = rewireModulesIncludes(config, env, modulesPaths);
+
+  config.resolve.symlinks = false;
 
   config.resolve.modules.unshift(...modulesPaths);
 
