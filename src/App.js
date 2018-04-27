0e2979d463d0271390a6a1dc8b52199f0d3aca7f
Bastien <bastien.alvez@gmail.com>
2018-04-27 09:46:42 +0200

    App is now minimal, start override with Main.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,23 @@
-import React, { Component } from 'react';
-
-import settings from 'front-settings';
+import React from 'react';
 
 import {
-  Breadcrumb,
   LocaleProvider,
-  Layout,
 } from 'antd';
 
 import fr from 'antd/lib/locale-provider/fr_FR';
 import 'moment/locale/fr';
 
-import Header from 'components/Header/Header';
-import SideMenu from 'components/SideMenu/SideMenu';
 import Main from 'components/Main/Main';
 
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { store, history } from './store';
 
-class App extends Component {
-  componentDidMount () {
-    // componentDidMount
-  }
-
-  render () {
-    return (
-      <Layout style={{ height: '100vh' }}>
-
-        <Header />
-
-        <Layout>
-          <Layout.Sider
-            breakpoint="md"
-            collapsedWidth="0"
-            style={{ background: 'white' }}
-          >
-            <SideMenu />
-          </Layout.Sider>
-
-          <Layout>
-
-            <Breadcrumb style={{ margin: '20px' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-              <Breadcrumb.Item>{settings.foo}</Breadcrumb.Item>
-            </Breadcrumb>
-
-            <Main />
-
-          </Layout>
-
-        </Layout>
-
-        <Layout.Footer>Footer content</Layout.Footer>
-
-      </Layout>
-    );
-  }
-}
-
 const AppWrapper = () => (
   <LocaleProvider locale={fr}>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <App />
+        <Main />
       </ConnectedRouter>
     </Provider>
   </LocaleProvider>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React,  { Component } from 'react';
 import { Route } from 'react-router-dom';
 
-import { Layout } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
 
 import Home from 'components/Home/Home';
 import About from 'components/About/About';
 
-const componentName = () => (
+import Header from 'components/Header/Header';
+import SideMenu from 'components/SideMenu/SideMenu';
+import settings from 'front-settings';
+
+class Main extends Component {
+  componentDidMount () {
+    // componentDidMount
+  }
+
+  render () {
+    return (
+      <Layout style={{ height: '100vh' }}>
+
+        <Header />
+
+        <Layout>
+          <Layout.Sider
+            breakpoint="md"
+            collapsedWidth="0"
+            style={{ background: 'white' }}
+          >
+            <SideMenu />
+          </Layout.Sider>
+
+          <Layout>
+
+            <Breadcrumb style={{ margin: '20px' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+              <Breadcrumb.Item>{settings.foo}</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <ComponentName />
+
+          </Layout>
+
+        </Layout>
+
+        <Layout.Footer>Footer content</Layout.Footer>
+
+      </Layout>
+    );
+  }
+}
+
+const ComponentName = () => (
   <Layout.Content style={{ margin: '0 20px', padding: '20px', background: 'white' }}>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
   </Layout.Content>
 );
 
-export default componentName;
+export default Main;
