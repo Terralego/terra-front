2a65cc4b4f2e93ca3cfdc84b69229e5cc19a3ba3
Alexandra Janin <contact.ajanin@gmail.com>
2018-04-27 12:43:12 +0200

    Fix lint warnings and errors on Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Form, Icon, Input, Button } from 'antd';
 import { addRequest } from '../../modules/base';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 const FormItem = Form.Item;
 
@@ -15,73 +15,66 @@ class NormalForm extends React.Component {
       }
     });
   };
-  render() {
+  render () {
     const { getFieldDecorator } = this.props.form;
     if (!this.props.request.name.length) {
       return (
-        <Form onSubmit={this.handleSubmit} className='login-form'>
+        <Form onSubmit={this.handleSubmit} className="login-form">
           <FormItem>
             {getFieldDecorator('name', {
               rules: [
-                { required: true, message: 'Veuillez saisir votre demande !' }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                placeholder='Nom de la demande'
-              />
-            )}
+                { required: true, message: 'Veuillez saisir votre demande !' },
+              ],
+            })(<Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Nom de la demande"
+            />)}
           </FormItem>
           <FormItem>
             {getFieldDecorator('activity', {
               rules: [
-                { required: true, message: "Veuillez saisir votre type d'actitivé !" }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                placeholder="Type d'activité"
-              />
-            )}
+                { required: true, message: "Veuillez saisir votre type d'actitivé !" },
+              ],
+            })(<Input
+              prefix={
+                <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+              }
+              placeholder="Type d'activité"
+            />)}
           </FormItem>
           <FormItem>
             <Button
-              type='primary'
-              htmlType='submit'
-              className='login-form-button'
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
             >
               Valider
             </Button>
           </FormItem>
         </Form>
       );
-    } else {
-      return (
-        <div>
-          <div>Nom de la demande : {this.props.request.name}</div>
-          <div>Type de l'activité : {this.props.request.activity}</div>
-        </div>
-      );
     }
+    return (
+      <div>
+        <div>Nom de la demande : {this.props.request.name}</div>
+        <div>Type : {this.props.request.activity}</div>
+      </div>
+    );
   }
 }
 
 const WrappedNormalLoginForm = Form.create()(NormalForm);
 
 const StateToProps = state => ({
-  request: state.base.request
+  request: state.base.request,
 });
 
 const DispatchToProps = dispatch =>
   bindActionCreators(
     {
-      addRequest
+      addRequest,
     },
-    dispatch
+    dispatch,
   );
 
 export default connect(StateToProps, DispatchToProps)(WrappedNormalLoginForm);
