dcd4a5a97b8233fa89ea1f2b01c8b71f5b302edb
Alexandra Janin <contact.ajanin@gmail.com>
2018-05-15 16:51:05 +0200

    Rename default project with title/description

--- src/components/Form/Form-steps.js
+++ src/components/Form/Form-steps.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Summary from 'components/Summary/Summary';
-import FormUser from 'components/Form/FormUser';
+import FormProperties from 'components/Form/FormProperties';
 
 const steps = [
-  { title: 'Utilisateur', component: <FormUser />, index: 0 },
-  { title: 'Récapitulatif', component: <Summary />, index: 4 },
+  { title: 'Projet', component: <FormProperties />, index: 0 },
+  { title: 'Récapitulatif', component: <Summary />, index: 1 },
 ];
 
 export default steps;
