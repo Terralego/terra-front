3342c7542277b0a9b0d99035c75e4af362775097
Florian Sommariva <florian.sommariva@makina-corpus.com>
2018-06-06 15:00:52 +0200

    Clean SVG code

--- src/components/MapDrawButtons/Polygon.js
+++ src/components/MapDrawButtons/Polygon.js
@@ -2,156 +2,66 @@ import React from 'react';
 
 export default props => (
   <svg
-    version="1.1"
-    id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    x="0px"
-    y="0px"
     viewBox="0 0 144 144"
-    xmlSpace="preserve"
-    width="20px"
-    height="20px"
   >
     <g fill={props.color || '#000000'}>
       <line
-        fill={props.color || '#000000'}
         stroke={props.color || '#000000'}
         strokeWidth="8"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
         x1="26.89"
         y1="37.505"
         x2="108.329"
         y2="16.034"
       />
       <line
-        fill={props.color || '#000000'}
         stroke={props.color || '#000000'}
         strokeWidth="8"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
         x1="108.329"
         y1="17.158"
         x2="126.376"
         y2="126.299"
       />
       <line
-        fill={props.color || '#000000'}
         stroke={props.color || '#000000'}
         strokeWidth="8"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
         x1="126.376"
         y1="127.391"
         x2="25.764"
         y2="105.985"
       />
       <line
-        fill={props.color || '#000000'}
         stroke={props.color || '#000000'}
         strokeWidth="8"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
         x1="24.311"
         y1="105.397"
         x2="27.217"
         y2="38.476"
       />
-    </g>
-    <g>
-      <g fill={props.color || '#000000'}>
-        <rect
-          x="110.198"
-          y="109.037"
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="27.84"
-          height="26.674"
-        />
-        <rect
-          x="115.967"
-          y="113.892"
-          fill={props.color || '#000000'}
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="17.306"
-          height="16.052"
-        />
-      </g>
-      <g fill={props.color || '#000000'}>
-        <rect
-          x="94.409"
-          y="3.821"
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="27.84"
-          height="26.674"
-        />
-        <rect
-          x="100.178"
-          y="8.675"
-          fill={props.color || '#000000'}
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="17.306"
-          height="16.052"
-        />
-      </g>
-      <g fill={props.color || '#000000'}>
-        <rect
-          x="13.297"
-          y="92.061"
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="27.84"
-          height="26.674"
-        />
-        <rect
-          x="19.065"
-          y="96.915"
-          fill={props.color || '#000000'}
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="17.306"
-          height="16.052"
-        />
-      </g>
-      <g fill={props.color || '#000000'}>
-        <rect
-          x="12.97"
-          y="25.139"
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="27.84"
-          height="26.674"
-        />
-        <rect
-          x="18.738"
-          y="29.994"
-          fill={props.color || '#000000'}
-          stroke={props.color || '#000000'}
-          strokeWidth="2"
-          strokeLinejoin="bevel"
-          strokeMiterlimit="10"
-          width="17.306"
-          height="16.052"
-        />
-      </g>
+      <rect
+        x="110.198"
+        y="109.037"
+        width="27.84"
+        height="26.674"
+      />
+      <rect
+        x="94.409"
+        y="3.821"
+        width="27.84"
+        height="26.674"
+      />
+      <rect
+        x="13.297"
+        y="92.061"
+        width="27.84"
+        height="26.674"
+      />
+      <rect
+        x="12.97"
+        y="25.139"
+        width="27.84"
+        height="26.674"
+      />
     </g>
   </svg>
 );
