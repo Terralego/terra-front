e69166a1090022e004964d90ee6c97b248d44d64
Hadrien Lanneau <hadrien@lanneau.me>
2018-11-09 17:15:30 +0100

    Build project

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
-
-it('should render', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
