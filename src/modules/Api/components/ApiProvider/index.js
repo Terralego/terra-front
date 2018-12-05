04ee097969c8233d8ccdbaa2d0dc5a387953f0af
Aymeric Guillien <aymericguillien@gmail.com>
2018-12-05 10:21:04 +0100

    Add ApiProvider component & tests

---
+++ src/modules/Api/components/ApiProvider/index.js
@@ -0,0 +1,8 @@
+import Api from '../../';
+
+export const ApiProvider = ({ host, children }) => {
+  Api.host = host;
+  return children;
+};
+
+export default ApiProvider;
--- src/modules/Api/index.js
+++ src/modules/Api/index.js
@@ -2,4 +2,7 @@ import Api from './services/api';
 
 export * from './services/api';
 export { default as log } from './services/log';
+
+export { default as ApiProvider } from './components/ApiProvider';
+
 export default Api;
