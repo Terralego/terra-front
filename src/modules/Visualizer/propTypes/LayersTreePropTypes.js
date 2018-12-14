632428269a5ffc044f7956506da5b0af06560ed4
Hadrien Lanneau <hadrien@lanneau.me>
2018-12-14 16:16:20 +0100

    Fix layers tree proptypes

--- src/modules/Visualizer/propTypes/LayersTreePropTypes.js
+++ src/modules/Visualizer/propTypes/LayersTreePropTypes.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 
-export const LayersTreeProps = PropTypes.arrayOf(PropTypes.shape({
+export const LayersTreeProps = PropTypes.shape({
   label: PropTypes.string.isRequired,
   layers: PropTypes.arrayOf(PropTypes.string),
   initialState: PropTypes.shape({
     active: PropTypes.bool,
   }),
-}));
+});
 
-export const LayersTreeGroupProps = PropTypes.arrayOf(PropTypes.shape({
+export const LayersTreeGroupProps = PropTypes.shape({
   group: PropTypes.string.isRequired,
-  layers: LayersTreeProps.isRequired,
-}));
+  layers: PropTypes.arrayOf(LayersTreeProps.isRequired),
+});
 
-export default PropTypes.oneOfType([LayersTreeProps, LayersTreeGroupProps]);
+export default PropTypes.arrayOf(PropTypes.oneOfType([
+  LayersTreeProps,
+  LayersTreeGroupProps,
+]));
