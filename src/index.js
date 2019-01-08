b93b70233d5bc9396f0e6d1865bf2468c1c73f15
marianne lajante <marianne.lajante@makina-corpus.com>
2019-01-08 10:09:56 +0100

    change modules default export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import TerraFrontProvider from './modules/TerraFrontProvider';
-import * as Api from './modules/Api';
-import * as Auth from './modules/Auth';
-import * as Map from './modules/Map/Map';
-import * as InterativeMap from './modules/Map/InteractiveMap';
+import Api from './modules/Api';
+import Auth from './modules/Auth';
+import Map from './modules/Map/Map';
+import InteractiveMap from './modules/Map/InteractiveMap';
 
-export { Api, Auth, Map, InterativeMap };
+export { Api, Auth, Map, InteractiveMap };
 
 export default TerraFrontProvider;
