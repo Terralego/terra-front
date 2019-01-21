a1ada6054744dd1dcd2683c49c568918d64be086
Hadrien Lanneau <hadrien@lanneau.me>
2019-01-21 15:17:16 +0100

    Multi level legends

--- src/modules/Map/InteractiveMap/components/Legend/Legend.js
+++ src/modules/Map/InteractiveMap/components/Legend/Legend.js
@@ -2,31 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-export const Legend = ({ title, items }) => (
-  <div className="tf-legend">
+export const Legend = ({ title, items, level = 1 }) => (
+  <div className={`tf-legend tf-legend--level-${level}`}>
     <h4
       className="tf-legend__title"
     >
       {title}
     </h4>
-    {items.map(({ label, color, shape = 'square' }) => (
-      <div
-        key={`${label}${color}`}
-        className={`tf-legend__item item item--${shape}`}
-      >
-        <div
-          className="item__before"
-          style={{ backgroundColor: color }}
-        />
-        <div className="item__label">
-          {label}
-        </div>
-        <div
-          className="item__after"
-          style={{ backgroundColor: color }}
-        />
-      </div>
-    ))}
+    {items.map(({ label, color, items: subItems, shape = 'square' }) => (
+      subItems
+        ? (
+          <Legend
+            key={`${label}${items.length}`}
+            title={label}
+            items={subItems}
+            level={level + 1}
+          />
+        ) : (
+          <div
+            key={`${label}${color}`}
+            className={`tf-legend__item item item--${shape}`}
+          >
+            <div
+              className="item__before"
+              style={{ backgroundColor: color }}
+            />
+            <div className="item__label">
+              {label}
+            </div>
+            <div
+              className="item__after"
+              style={{ backgroundColor: color }}
+            />
+          </div>
+        )))}
   </div>
 );
 
@@ -34,7 +43,8 @@ Legend.propTypes = {
   title: PropTypes.string.isRequired,
   items: PropTypes.arrayOf(PropTypes.shape({
     label: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
+    color: PropTypes.string,
+    items: PropTypes.array,
     shape: PropTypes.string,
   })).isRequired,
 };
