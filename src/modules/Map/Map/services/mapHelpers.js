c587236ea633738eafb4074bb406546b1ec1dbb5
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-02-14 14:05:11 +0100

    Create `addMapDebug` helper for debugging vector tiles features

---
+++ src/modules/Map/Map/services/mapHelpers.js
@@ -0,0 +1,45 @@
+import mapBoxGl from 'mapbox-gl';
+import MapboxInspect from 'mapbox-gl-inspect';
+import renderInspectPopup from 'mapbox-gl-inspect/lib/renderPopup';
+import 'mapbox-gl-inspect/dist/mapbox-gl-inspect.css';
+
+export const addMapDebug = map => {
+  const mapDebugMode = global.localStorage.getItem('mapDebug');
+  const debugToPopup = ['*', 'popup'].includes(mapDebugMode);
+  const debugToConsole = ['*', 'console'].includes(mapDebugMode);
+
+  if (debugToPopup || debugToConsole) {
+    const popup = new mapBoxGl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    if (!debugToPopup) {
+      popup.on('open', ({ target }) => target.remove());
+    }
+
+    const renderPopup = features => {
+      if (debugToConsole) {
+        console.log(features); // eslint-disable-line no-console
+      }
+      return renderInspectPopup(features);
+    };
+
+    map.addControl(new MapboxInspect({
+      popup,
+      renderPopup,
+      showMapPopup: true,
+      showMapPopupOnHover: false,
+
+      showInspectButton: false,
+      showInspectMap: false,
+      showInspectMapPopupOnHover: false,
+    }));
+  }
+
+  return map;
+};
+
+export default {
+  addMapDebug,
+};
