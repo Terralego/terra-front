ab4cc94270433e1bc826122cda3e2e9f53f49c8b
Aymeric Guillien <aymericguillien@gmail.com>
2019-02-20 10:44:15 +0100

    Add checkboxes control

--- src/modules/Forms/Controls/Checkboxes.js
+++ src/modules/Forms/Controls/Checkboxes.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import { Checkbox } from '@blueprintjs/core';
 
-export const Checkboxes = () => <p>Checkboxes</p>;
+
+export class Checkboxes extends React.Component {
+  state = {
+    values: [],
+  }
+
+  handleChange = value => async () => {
+    const { onChange } = this.props;
+    const { values } = this.state;
+    await this.setState({
+      values: values.includes(value)
+        ? [...values.filter(val => val !== value)]
+        : [...values, value],
+    });
+    // eslint-disable-next-line
+    onChange(this.state.values);
+  }
+
+  render () {
+    const { label, values: valuesProperties } = this.props;
+    const { handleChange } = this;
+
+    return (
+      <div>
+        <p>{label}</p>
+        {valuesProperties.map(val =>
+          <Checkbox key={val} onChange={handleChange(val)} label={val} />)}
+      </div>
+    );
+  }
+}
 
 export default Checkboxes;
