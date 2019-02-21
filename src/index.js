955594a1cccb35af33b5419b66379a742e42ee3c
Hadrien Lanneau <hadrien@lanneau.me>
2019-02-21 09:53:17 +0100

    Bump new version
    - Table component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import TerraFrontProvider from './modules/TerraFrontProvider';
+
 import Api from './modules/Api';
+
 import Auth from './modules/Auth';
+
 import Map from './modules/Map/Map';
 import InteractiveMap from './modules/Map/InteractiveMap';
 
-export { Api, Auth, Map, InteractiveMap };
+import Table from './modules/Table/Table';
+
+export { Api, Auth, Map, InteractiveMap, Table };
 
 export default TerraFrontProvider;
