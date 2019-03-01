1cf4a311e9e5a410fa43300e7cef8a04fc448b82
marianne lajante <marianne.lajante@makina-corpus.com>
2019-03-01 15:39:21 +0100

    write test

---
+++ src/modules/Table/tableUtils.test.js
@@ -0,0 +1,8 @@
+import { getRows } from './tableUtils';
+
+it('should get rows', () => {
+  const data = [['foo', 'foo'], ['bar', 'bar']];
+  expect(getRows([{ cols: [0, 0], rows: [0, 0] }], data)).toEqual([0]);
+  expect(getRows([{ cols: [0, 0], rows: [1, 1] }], data)).toEqual([1]);
+  expect(getRows([{ cols: [1, 1], rows: [0, 1] }], data)).toEqual([0, 1]);
+});
