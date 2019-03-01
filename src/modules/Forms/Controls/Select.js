b5b4f522de7dd470f7c92c59e2ae571b88a90552
Hadrien Lanneau <hadrien@lanneau.me>
2019-03-01 08:42:05 +0100

    Select empty value

--- src/modules/Forms/Controls/Select.js
+++ src/modules/Forms/Controls/Select.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { MenuItem, Button } from '@blueprintjs/core';
 import { Select as BPSelect } from '@blueprintjs/select';
 
@@ -20,35 +21,77 @@ export class Select extends React.Component {
     onChange () {},
   }
 
-  handleChange = value => {
+  state = {
+    items: [],
+  }
+
+  componentDidMount () {
+    this.updateItems();
+  }
+
+  componentDidUpdate ({ values: prevValues }) {
+    const { values } = this.props;
+
+    if (values !== prevValues) {
+      this.updateItems();
+    }
+  }
+
+  handleChange = ({ value }) => {
     const { onChange } = this.props;
     onChange(value);
   }
 
-  handleClick = value => () => this.handleChange(value);
+  updateItems () {
+    const { values, locales: { emptySelectItem } } = this.props;
+    this.setState({
+      items: ['', ...values].map(v => ({
+        // TODO changer RIEN par une locale venant de Filters
+        label: (v === '' ? emptySelectItem : v),
+        value: v,
+      })),
+    });
+  }
 
   render () {
     const {
       label,
       values,
-      values: [defaultValue],
       locales: { noResults },
       value,
     } = this.props;
-    const { handleChange, handleClick } = this;
+    const { items } = this.state;
+    const { handleChange } = this;
 
     return (
       <div className="control-container">
         <p className="control-label">{label}</p>
         <BPSelect
           popoverProps={{ usePortal: false }}
-          items={values}
+          items={items}
           filterable={values.length > 9}
-          itemRenderer={val => <MenuItem onClick={handleClick(val)} key={val} text={val} />}
+          itemRenderer={({
+            label: itemLabel, value: itemValue,
+          }, {
+            handleClick, modifiers: { matchesPredicate, ...modifiers },
+          }) => (
+            <div className={classnames({
+              'control-container__item': true,
+              'control-container__item--empty': itemValue === '',
+            })}
+            >
+              <MenuItem
+                {...modifiers}
+                onClick={handleClick}
+                key={itemValue}
+                text={itemLabel}
+              />
+            </div>
+          )}
           noResults={<MenuItem disabled text={noResults} />}
           onItemSelect={handleChange}
         >
-          <Button text={value || defaultValue} rightIcon="double-caret-vertical" />
+          <Button text={value} rightIcon="double-caret-vertical" />
         </BPSelect>
       </div>
     );
