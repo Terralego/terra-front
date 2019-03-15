b6524defdcbf88f376d8cf9337acadb827131f9a
marianne lajante <marianne.lajante@makina-corpus.com>
2019-03-15 15:39:24 +0100

    add radius props for circle legends

--- src/modules/Map/InteractiveMap/components/Legend/Legend.js
+++ src/modules/Map/InteractiveMap/components/Legend/Legend.js
@@ -2,42 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-export const Legend = ({ title, items, level = 1 }) => (
-  <div className={`tf-legend tf-legend--level-${level}`}>
-    <h4
-      className="tf-legend__title"
-    >
-      {title}
-    </h4>
-    {items.map(({ label, color, items: subItems, shape = 'square' }) => (
-      subItems
-        ? (
-          <Legend
-            key={`${label}${items.length}`}
-            title={label}
-            items={subItems}
-            level={level + 1}
-          />
-        ) : (
-          <div
-            key={`${label}${color}`}
-            className={`tf-legend__item item item--${shape}`}
-          >
-            <div
-              className="item__before"
-              style={{ backgroundColor: color }}
-            />
-            <div className="item__label">
-              {label}
-            </div>
-            <div
-              className="item__after"
-              style={{ backgroundColor: color }}
-            />
-          </div>
-        )))}
-  </div>
-);
+export const Legend = props => {
+  const { title, items, level, position } = props;
+  const biggerRadius = items.filter((int, item) => Math.max(int, item.radius), 0);
+
+  return (
+    <div className={`tf-legend tf-legend--level-${level}`}>
+      <h4
+        className="tf-legend__title"
+      >
+        {title}
+      </h4>
+      <div className="tf-legend__list">
+        {items.map(({ label, color, items: subItems, shape = 'square', radius }) => (
+          subItems
+            ? (
+              <Legend
+                key={`${label}${items.length}`}
+                title={label}
+                items={subItems}
+                level={level + 1}
+              />
+            ) : (
+              <div
+                key={`${label}${color}`}
+                className={`tf-legend__item item--${shape} item--${position}`}
+              >
+                <div
+                  className="item__symbol-container"
+                  style={{ height: `${biggerRadius}px`, margin: '0.5rem' }}
+                >
+                  <div
+                    className="item__symbol"
+                    style={{ backgroundColor: color, width: radius, height: radius }}
+                  />
+                </div>
+                <div
+                  className="item__label"
+                >
+                  {label}
+                </div>
+              </div>
+            )))}
+      </div>
+    </div>
+
+  );
+};
 
 Legend.propTypes = {
   title: PropTypes.string.isRequired,
@@ -46,7 +57,14 @@ Legend.propTypes = {
     color: PropTypes.string,
     items: PropTypes.array,
     shape: PropTypes.string,
+    radius: PropTypes.number,
   })).isRequired,
+  position: PropTypes.string,
+  level: PropTypes.number,
+};
+Legend.defaultProps = {
+  position: 'left',
+  level: 1,
 };
 
 export default Legend;
