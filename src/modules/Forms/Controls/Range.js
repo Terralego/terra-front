9096eef87dc2751cca2e2af428e160c67ec084b4
Hadrien Lanneau <hadrien@lanneau.me>
2019-03-25 17:25:26 +0100

    Manual set range values

--- src/modules/Forms/Controls/Range.js
+++ src/modules/Forms/Controls/Range.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { RangeSlider } from '@blueprintjs/core';
+import ContentEditable from 'react-contenteditable';
 
 import './index.scss';
 
 const DEFAULT_MIN = 0;
 const DEFAULT_MAX = 100;
+
+const handleManualChange = ({
+  onChange, prevMin, prevMax, newMin, newMax, min, max,
+}) => {
+  if (newMin !== undefined) {
+    onChange([Math.min(max, Math.min(newMin, prevMax)), prevMax]);
+  }
+  if (newMax !== undefined) {
+    onChange([prevMin, Math.max(min, Math.max(prevMin, newMax))]);
+  }
+};
+
+const selectAll = ({ target }) => {
+  const selection = global.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(target);
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
 
 export const Range = ({
   label,
@@ -15,17 +35,70 @@ export const Range = ({
   max,
   ...props
 }) => {
-  if (value && (value.length !== 2 || (+value[0] > +value[1]))) {
+  const [minValue, maxValue] = value || [min, max];
+
+  if (value && (value.length !== 2 || (+minValue > +maxValue))) {
     throw new Error('Range control: There must be two values and the first must be less than the second');
   }
+
+  const [newMin, setMin] = useState();
+  const [newMax, setMax] = useState();
+  const [blured, setBlured] = useState(false);
+
+  useEffect(() => {
+    if (blured) {
+      handleManualChange({
+        onChange,
+        prevMin: minValue,
+        prevMax: maxValue,
+        newMin,
+        newMax,
+        min,
+        max,
+      });
+      setMin();
+      setMax();
+      setBlured(false);
+    }
+  }, [blured]);
+
   return (
-    <div className="control-container">
+    <div className="control-container control--range range">
       <p className="control-label">{label}</p>
       <RangeSlider
         value={value || [min, max]}
         onChange={onChange}
         min={min}
         max={max}
+        labelRenderer={itemValue => {
+          if (+itemValue === minValue || +itemValue === maxValue) {
+            const minChanged = +itemValue === minValue;
+            const maxChanged = !minChanged;
+            const inputValue = minChanged
+              ? newMin
+              : newMax;
+            return (
+              <ContentEditable
+                className="range__manual"
+                onChange={({ target: { value: newValue } }) => {
+                  const fixedNewValue = Number.isNaN(+newValue)
+                    ? 0
+                    : +newValue;
+                  if (minChanged) {
+                    setMin(fixedNewValue);
+                  }
+                  if (maxChanged) {
+                    setMax(fixedNewValue);
+                  }
+                }}
+                html={`${inputValue === undefined ? itemValue : inputValue}`}
+                onBlur={() => setBlured(true)}
+                onFocus={selectAll}
+              />
+            );
+          }
+          return itemValue;
+        }}
         {...props}
       />
     </div>
