f4fc294de705f85cfb464cfa5b1034f475b5d951
Hadrien Lanneau <hadrien@lanneau.me>
2019-04-11 09:16:11 +0200

    Upgrade storybook to 5.x

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,10 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { configureViewport } from '@storybook/addon-viewport';
 
 function loadStories() {
   require('../src/stories');
 }
 
 addDecorator(withKnobs);
-configureViewport();
 
 configure(loadStories, module);
