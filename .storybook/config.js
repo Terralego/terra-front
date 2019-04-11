ca10a0ace8c47fb177d3a98568dd09bca3b2171a
Hadrien Lanneau <hadrien@lanneau.me>
2019-04-11 09:21:12 +0200

    Add a11y addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,12 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
 function loadStories() {
   require('../src/stories');
 }
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
 
 configure(loadStories, module);
