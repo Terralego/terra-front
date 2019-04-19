b02ce71a89fe9473e542f19a553772ba99e9b669
Hadrien Lanneau <hadrien@lanneau.me>
2019-04-19 17:53:39 +0200

    Display clean source

--- src/stories/modules/Map/index.js
+++ src/stories/modules/Map/index.js
@@ -1,13 +1,11 @@
 import { storiesOf } from '@storybook/react';
 
-import MapView from './Map';
 import InteractiveMap from './InteractiveMap';
 import SyncedMaps from './SyncedMaps';
 import MapWithCluster from './MapWithCluster';
 
 const stories = storiesOf('Modules/Map/', module);
 
-stories.add('Map', MapView);
 stories.add('InteractiveMap', InteractiveMap);
 stories.add('SyncedMaps', SyncedMaps);
 stories.add('Map with cluster', MapWithCluster);
