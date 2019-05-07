94f42272f374dd1be93e57aa545eaabcb22aa223
Hadrien Lanneau <hadrien@lanneau.me>
2019-05-07 10:19:11 +0200

    Design enhancements on search control

--- src/modules/Map/Map/components/SearchResults/SearchResults.js
+++ src/modules/Map/Map/components/SearchResults/SearchResults.js
@@ -15,54 +15,56 @@ export const SearchResults = ({
     <p className="search-results__title">
       {translate('terralego.map.search_results.title')}
     </p>
-    {results.map(({ group, total, results: resultsItems }, index) => (
-      <div
-        key={group}
-        className="search-results__group"
-      >
-        <p className="search-results__group-title">
-          {group}
-          {total && (
-            <>
-              &nbsp;
-              <em>
-                {translate('terralego.map.search_results.group_total', { count: total })}
-              </em>
-            </>
-          )}
-        </p>
-        {resultsItems.length
-          ? (
-            <ul className="search-results__list">
-              {resultsItems.slice(0, 5).map(item => (
-                <li
-                  key={item.label}
-                  className={classnames({
-                    'search-results__item': true,
-                    'search-results__item--active': true,
-                    'search-results__item--selected': selected === item,
-                  })}
-                >
-                  <button
-                    type="button"
-                    onClick={() => onClick(item)}
+    <div className="search-results__groups">
+      {results.map(({ group, total, results: resultsItems }, index) => (
+        <div
+          key={group}
+          className="search-results__group"
+        >
+          <p className="search-results__group-title">
+            {group}
+            {total && (
+              <>
+                &nbsp;
+                <em>
+                  {translate('terralego.map.search_results.group_total', { count: total })}
+                </em>
+              </>
+            )}
+          </p>
+          {resultsItems.length
+            ? (
+              <ul className="search-results__list">
+                {resultsItems.slice(0, 5).map(item => (
+                  <li
+                    key={item.id}
+                    className={classnames({
+                      'search-results__item': true,
+                      'search-results__item--active': true,
+                      'search-results__item--selected': selected === item,
+                    })}
                   >
-                    {item.label}
-                  </button>
-                </li>
-              ))}
-              {resultsItems.length > maxResults && (
-                <li className="search-results__item search-results__item--more">…</li>
-              )}
-            </ul>
-          ) : (
-            <p className="search-results__item">
-              {translate('terralego.map.search_results.no_result')}
-            </p>
-          )}
-        {!!results[index + 1] && <Divider />}
-      </div>
-    ))}
+                    <button
+                      type="button"
+                      onClick={() => onClick(item)}
+                    >
+                      {item.label}
+                    </button>
+                  </li>
+                ))}
+                {resultsItems.length > maxResults && (
+                  <li className="search-results__item search-results__item--more">…</li>
+                )}
+              </ul>
+            ) : (
+              <p className="search-results__item">
+                {translate('terralego.map.search_results.no_result')}
+              </p>
+            )}
+          {!!results[index + 1] && <Divider />}
+        </div>
+      ))}
+    </div>
   </Card>
 );
 
