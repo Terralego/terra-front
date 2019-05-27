58608f9324a3afa8864d0d498f849f8231205ae4
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-05-27 15:04:16 +0000

    Control capture

--- src/modules/Map/Map/Map.test.js
+++ src/modules/Map/Map/Map.test.js
@@ -540,6 +540,19 @@ describe('controls', () => {
     expect(map.addControl).toHaveBeenCalledWith(instance.controls[0], 'top-right');
   });
 
+  it('should update capture control state', () => {
+    const instance = new Map({});
+    instance.props = {
+      map,
+      controls: [{
+        control: 'CaptureControl',
+        position: 'top-right',
+      }],
+    };
+    instance.resetControls();
+    expect(map.addControl).toHaveBeenCalledWith(instance.controls[0], 'top-right');
+  });
+
   it('should focus on search result', () => {
     const instance = new Map({ map });
 
---
+++ src/modules/Map/Map/components/CaptureControl/CaptureControl.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from '@blueprintjs/core';
+import { saveAs } from 'file-saver';
+
+import AbstractMapControl from '../../../helpers/AbstractMapControl';
+import translateMock from '../../../../../utils/translate';
+
+
+export class CaptureControl extends AbstractMapControl {
+  static containerClassName = 'mapboxgl-ctrl mapboxgl-ctrl-group mapboxgl-ctrl-capture';
+
+  static propTypes = {
+    /** Function used to translate wording. Takes key and object of options as parameters */
+    translate: PropTypes.func,
+  }
+
+  static defaultProps = {
+    translate: translateMock,
+  }
+
+  captureScreen = async () => {
+    const { map } = this.props;
+    const blob = await new Promise(resolve => map.getCanvas().toBlob(resolve));
+    saveAs(blob, 'map_screenshot.png');
+  }
+
+  render () {
+    const { translate } = this.props;
+    return (
+      <button
+        className="mapboxgl-capture"
+        type="button"
+        onClick={this.captureScreen}
+        title={translate('terralego.map.capture_control.button_label')}
+        aria-label={translate('terralego.map.capture_control.button_label')}
+      >
+        <Icon icon="download" />
+      </button>
+    );
+  }
+}
+
+export default CaptureControl;
