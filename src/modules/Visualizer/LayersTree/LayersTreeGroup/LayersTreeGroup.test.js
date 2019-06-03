75d4436eebc169b3f4ec10ce463fba952322617c
marianne lajante <marianne.lajante@makina-corpus.com>
2019-06-03 14:52:40 +0200

    change from hla remarks

--- src/modules/Visualizer/LayersTree/LayersTreeGroup/LayersTreeGroup.test.js
+++ src/modules/Visualizer/LayersTree/LayersTreeGroup/LayersTreeGroup.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
 import LayersTreeGroup from './LayersTreeGroup';
 
 jest.mock('@blueprintjs/core', () => ({
@@ -45,4 +47,47 @@ it('should render correctly', () => {
     </>
   )).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('should change layers group state', () => {
+  const wrapper = shallow((
+    <LayersTreeGroup
+      title="Group 1"
+      layer={{
+        layers: [{
+          label: 'Layer 1',
+        }, {
+          label: 'Layer 2',
+        }],
+      }}
+      isOpen
+    />
+  ));
+  expect(wrapper.state().isOpen).toEqual(true);
+  wrapper.setState({
+    isOpen: false,
+  });
+  expect(wrapper.state().isOpen).toEqual(false);
+});
+
+it('should open group panel', () => {
+  const instance = new LayersTreeGroup({});
+  instance.setState = jest.fn();
+
+  instance.state = {
+    isOpen: true,
+  };
+  instance.handleClick();
+  expect(instance.setState).toHaveBeenCalledWith({
+    isOpen: false,
+  });
+  instance.setState.mockClear();
+
+  instance.state = {
+    isOpen: false,
+  };
+  instance.handleClick();
+  expect(instance.setState).toHaveBeenCalledWith({
+    isOpen: true,
+  });
 });
