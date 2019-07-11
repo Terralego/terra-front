075439cc6f294835890af784a0c73ac9418df178
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-07-11 08:54:22 +0200

    Fix a small typo

--- src/stories/summary.js
+++ src/stories/summary.js
@@ -7,6 +7,6 @@ const stories = storiesOf('Summary', module);
 stories.add('Read me', () => (
   <div>
     <h1>Terra Front Storybook</h1>
-    <p>Some presentation and documentation will comes here later</p>
+    <p>Some presentation and documentation will come here later</p>
   </div>
 ));
