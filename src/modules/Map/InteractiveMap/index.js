b31c98c1036e63fa44648e29df44b7a6e166cb09
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-07-12 17:54:04 +0200

    Remove useless HOC connectTerraFrontProvider from InteractiveMap
    ref #126

--- src/modules/Map/InteractiveMap/index.js
+++ src/modules/Map/InteractiveMap/index.js
@@ -1,13 +1,5 @@
-import React from 'react';
-import { connectTerraFrontProvider } from '../../TerraFrontProvider';
 import InteractiveMap from './InteractiveMap';
 
 export * from './InteractiveMap';
-export default connectTerraFrontProvider({
-  renderInteractiveMap: 'modules.Map.components.renderInteractiveMap',
-})(
-  ({
-    renderInteractiveMap: RenderInteractiveMap = InteractiveMap,
-    ...props
-  }) => <RenderInteractiveMap defaultRender={InteractiveMap} {...props} />,
-);
+
+export default InteractiveMap;
