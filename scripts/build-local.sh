a913d8a7b4ea64c0884a186b8e18a86651c990ed
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-07-19 12:04:57 +0200

    Replace build-local.sh & publish.sh with npm scripts

--- scripts/build-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-npm run build
-cp package.json dist
-cp package-lock.json dist
-cp README.md dist
--- scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-npm run build
-cp package.json dist
-cp package-lock.json dist
-cp README.md dist
-cd dist
-npm publish --access public
