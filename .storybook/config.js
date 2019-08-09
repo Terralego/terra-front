4b410239d9b8381049b81cfbd3518c9c51d4996d
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-08-09 17:08:35 +0200

    Add breathing space in Storybook config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,21 +3,22 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 import { withInfo} from '@storybook/addon-info';
 import ThemeSwitcher from './ThemeSwitcher';
+
 import './styles.scss';
-function loadStories() {
+
+const loadStories = () => {
   require('../src/stories');
+
   const req = require.context('../src/stories', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
 // It is important to declare this decorator as the first decorator, otherwise it won't work well.
 // https://github.com/storybooks/storybook/tree/next/addons/info
-addDecorator(withInfo({
-  source: true
-}));
+addDecorator(withInfo({ source: true }));
+
 addDecorator(withKnobs);
 addDecorator(withA11y);
 addDecorator(ThemeSwitcher);
-
 
 configure(loadStories, module);
