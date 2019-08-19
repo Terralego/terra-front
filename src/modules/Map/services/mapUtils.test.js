9c7ef6ded8a86ef09245ce11dba8d604f996a590
Hadrien Lanneau <hadrien@lanneau.me>
2019-08-19 11:53:21 +0200

    Revert "Merge branch 'fix-coverage' into 'master'"
    This reverts commit 2f290e1083f9e96fb9872760887ee49a380a217e, reversing
    changes made to b3c820449d7b039803c2f520d48fd4391e6ee112.

--- src/modules/Map/services/mapUtils.test.js
+++ src/modules/Map/services/mapUtils.test.js
@@ -335,7 +335,7 @@ describe('should set interactions', () => {
     map.queryRenderedFeatures = () => [];
 
     setInteractions({ map, interactions, callback });
-    listeners[2].listener(event);
+    listeners[0].listener(event);
     jest.runAllTimers();
 
     expect(callback).not.toHaveBeenCalled();
@@ -398,6 +398,11 @@ describe('should set interactions', () => {
       interaction: 'doSomething',
     }];
     const callback = () => {};
+    map.queryRenderedFeatures = () => [{
+      layer: {
+        id: 'foo',
+      },
+    }];
 
     setInteractions({ map, interactions, callback });
 
@@ -407,9 +412,7 @@ describe('should set interactions', () => {
     expect(map.getCanvas).toHaveBeenCalled();
     expect(canvas.style.cursor).toBe('pointer');
 
-    listeners[0].listener({ target: map, point: [1, 2] });
-    listeners[1].listener({ target: map, point: [1, 2] });
-    expect(canvas.style.cursor).toBe('pointer');
+    map.queryRenderedFeatures = () => [];
 
     listeners[1].listener({ target: map, point: [1, 2] });
     jest.runAllTimers();
