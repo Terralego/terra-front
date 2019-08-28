7a1c4b029228dddadab557459666e84476d2689e
Hadrien Lanneau <hadrien.lanneau@makina-corpus.com>
2019-08-28 10:16:47 +0200

    Connect translate function

--- src/modules/Visualizer/LayersTree/LayersTreeItem/LayersTreeItemOptions/index.js
+++ src/modules/Visualizer/LayersTree/LayersTreeItem/LayersTreeItemOptions/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { connectLayersTree } from '../../LayersTreeProvider/context';
 import LayersTreeItemOptionsDesktop from './LayersTreeItemOptionsDesktop';
 import LayersTreeItemOptionsTablet from './LayersTreeItemOptionsTablet';
 import withDeviceSize from '../../../../../utils/withDeviceSize';
 
-export const LayersTreeItemOptions  = ({ isMobileSized, isPhoneSized, ...props }) => (
+export const LayersTreeItemOptions  = connectLayersTree('translate')(({ isMobileSized, isPhoneSized, ...props }) => (
   isMobileSized
     ? <LayersTreeItemOptionsTablet {...props} />
     : <LayersTreeItemOptionsDesktop {...props} />
-);
+));
 export default withDeviceSize()(LayersTreeItemOptions);
--- src/modules/Visualizer/LayersTree/LayersTreeItem/WarningZoom/index.js
+++ src/modules/Visualizer/LayersTree/LayersTreeItem/WarningZoom/index.js
@@ -1,3 +1,4 @@
 import WarningZoom from './WarningZoom';
+import { connectLayersTree } from '../../LayersTreeProvider/context';
 
-export default WarningZoom;
+export default connectLayersTree('translate')(WarningZoom);
