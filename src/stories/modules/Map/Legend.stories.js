29d44f7fcb358f0f4f83520726afb31b96b04402
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-08-28 18:12:44 +0200

    Create stories for Legend component

---
+++ src/stories/modules/Map/Legend.stories.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+
+import Legend from '../../../modules/Map/InteractiveMap/components/Legend';
+
+const json = [
+  {
+    label: 'level1.1',
+    color: 'red',
+  },
+  {
+    label: 'level1.2',
+    color: 'blue',
+  },
+  {
+    label: 'level2',
+    items: [
+      {
+        label: 'level2.1',
+        color: 'red',
+      },
+      {
+        label: 'level2.2',
+        color: 'blue',
+      },
+      {
+        label: 'level3',
+        items: [
+          {
+            label: 'level3.1',
+            color: 'red',
+          },
+          {
+            label: 'level3.2',
+            color: 'blue',
+          },
+          {
+            label: 'level4',
+            items: [
+              {
+                label: 'level4.1',
+                color: 'red',
+              },
+              {
+                label: 'level4.2',
+                color: 'blue',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const template = `# Hello World
+
+<square color="red" /> foo<br />
+<square color="green" /> bar<br />
+
+<circle color="red" /> foo<br />
+<circle color="green" /> bar<br />
+`;
+
+const stories = storiesOf('Components/Legend', module);
+
+const knobsOptions = {
+  knobs: {
+    timestamps: true, // Doesn't emit events while user is typing.
+    escapeHTML: false,
+  },
+};
+
+stories.add('JSON items', () => (
+  <div style={{ width: 300, margin: '0 auto' }}>
+    <Legend
+      title="foo"
+      items={json}
+    />
+  </div>
+), knobsOptions);
+
+stories.add('Nunjucks template', () => (
+  <div style={{ width: 300, margin: '0 auto' }}>
+    <Legend
+      title="bar"
+      content={text('Nunjucks template', template)}
+    />
+  </div>
+), knobsOptions);
+
+stories.add('Both', () => (
+  <div style={{ width: 300, margin: '0 auto' }}>
+    <Legend
+      title="baz"
+      items={json}
+      content={text('Nunjucks template', template)}
+    />
+  </div>
+), knobsOptions);
