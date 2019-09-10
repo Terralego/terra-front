659e4e2932f2252d8118c07cfb3bfd36b048b450
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2019-09-10 10:21:08 +0200

    Convert Checkbox to functional component

--- src/modules/Forms/Controls/Checkboxes/Checkbox.js
+++ src/modules/Forms/Controls/Checkboxes/Checkbox.js
@@ -2,31 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Checkbox as BPCheckbox } from '@blueprintjs/core';
 
-export class Checkbox extends React.Component {
-  static propTypes = {
-    value: PropTypes.string,
-    label: PropTypes.string,
-    checked: PropTypes.bool,
-    onToggle: PropTypes.func,
-  };
+const Checkbox = ({ checked, label, onToggle, value }) => (
+  <BPCheckbox
+    onChange={() => onToggle(value)}
+    label={label || value}
+    checked={checked}
+  />
+);
 
-  static defaultProps = {
-    onToggle () {},
-    checked: false,
-    value: '',
-    label: '',
-  };
+Checkbox.propTypes = {
+  value: PropTypes.string,
+  label: PropTypes.string,
+  checked: PropTypes.bool,
+  onToggle: PropTypes.func,
+};
 
-  onToggle = () => {
-    const { onToggle, value } = this.props;
-    onToggle(value);
-  }
-
-  render () {
-    const { value, label, checked } = this.props;
-    const { onToggle } = this;
-    return <BPCheckbox onChange={onToggle} label={label || value} checked={checked} />;
-  }
-}
+Checkbox.defaultProps = {
+  value: '',
+  label: '',
+  checked: false,
+  onToggle () {},
+};
 
 export default Checkbox;
