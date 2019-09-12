b6c8ea9104ac2037a8ca2cf96b5c4511a2b41426
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2019-09-12 17:39:50 +0200

    Fix connect HOC on FilterPanelContent

--- src/modules/Visualizer/LayersTree/LayersTreeItem/FiltersPanel/FiltersPanelContent/index.js
+++ src/modules/Visualizer/LayersTree/LayersTreeItem/FiltersPanel/FiltersPanelContent/index.js
@@ -2,7 +2,9 @@ import FiltersPanelContent from './FiltersPanelContent';
 
 import { connectLayersTree } from '../../../LayersTreeProvider/context';
 
-export default connectLayersTree('translate', ({ getLayerState }, { layer, layer: { exclusive, layers } }) => {
+export default connectLayersTree((context, props) => {
+  const { getLayerState, translate } = context;
+  const { layer, layer: { exclusive, layers } } = props;
   const activeLayer = exclusive
     ? layers.find(l => getLayerState({ layer: l }).active) || layer
     : layer;
@@ -10,5 +12,6 @@ export default connectLayersTree('translate', ({ getLayerState }, { layer, layer
   return {
     filtersValues: getLayerState({ layer: activeLayer }).filters || {},
     activeLayer,
+    translate,
   };
 })(FiltersPanelContent);
