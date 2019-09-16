937a85cfa9cafa298622259e563c1fe467158ed5
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2019-09-16 09:53:23 +0200

    DX: document parameters

--- src/modules/Visualizer/LayersTree/LayersTreeItem/FiltersPanel/FiltersPanelContent/index.js
+++ src/modules/Visualizer/LayersTree/LayersTreeItem/FiltersPanel/FiltersPanelContent/index.js
@@ -2,9 +2,10 @@ import FiltersPanelContent from './FiltersPanelContent';
 
 import { connectLayersTree } from '../../../LayersTreeProvider/context';
 
-export default connectLayersTree((context, props) => {
-  const { getLayerState, translate } = context;
-  const { layer, layer: { exclusive, layers } } = props;
+export default connectLayersTree((
+  { getLayerState, translate }, // context
+  { layer, layer: { exclusive, layers } }, // props
+) => {
   const activeLayer = exclusive
     ? layers.find(l => getLayerState({ layer: l }).active) || layer
     : layer;
