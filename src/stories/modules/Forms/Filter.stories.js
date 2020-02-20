8c2f92a3a6ae6366a66c963eaf3c0df540f46fb7
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2020-02-20 11:54:25 +0100

    Fix story book with action and state

--- src/stories/modules/Forms/Filter.stories.js
+++ src/stories/modules/Forms/Filter.stories.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+
 import {
   text,
   select,
+  object,
   number,
   boolean,
   optionsKnob,
@@ -19,25 +21,25 @@ import './styles.css';
 import translateMock from '../../../utils/translate';
 
 const dummyValues = {
-  developpeur: 'développeur',
+  Developper: 'Developper',
   'UX designer': 'UX designer',
-  'Chef de projet': 'Chef de projet',
+  'Project manager': 'Project manager',
 };
 
-const dummyMonth = {
-  January: 'January',
-  February: 'February',
-  March: 'March',
-  April: 'April',
-  May: 'May',
-  June: 'June',
-  July: 'July',
-  August: 'August',
-  September: 'September',
-  October: 'October',
-  November: 'November',
-  December: 'December',
-};
+const dummyMonth = [
+  { label: 'January', value: '1' },
+  { label: 'February', value: '2' },
+  { label: 'March', value: '3' },
+  { label: 'April', value: '4' },
+  { label: 'May', value: '5' },
+  { label: 'June', value: '6' },
+  { label: 'July', value: '7' },
+  { label: 'August', value: '8' },
+  { label: 'September', value: '9' },
+  { label: 'October', value: '10' },
+  { label: 'November', value: '11' },
+  { label: 'December', value: '12' },
+];
 
 const translateMockWithDefaultValue = translateMock({
   'terralego.forms.controls.select.empty_item': 'All',
@@ -50,140 +52,156 @@ const translateMockWithDefaultValue = translateMock({
   'terralego.forms.controls.generic.empty_item': 'Nothing',
 });
 
-const onChange = properties => {
-  action('Filters update', properties);
+// Component used to manage state
+const Parent = ({ children, initData, ...props }) => {
+  const [state, setState] = React.useState(initData);
+  return <div>{children(state, setState)}</div>;
 };
 
 storiesOf('Components/Filters', module).add('Single', () => (
-  <Filters
-    translate={translateMockWithDefaultValue}
-    onChange={onChange}
-    properties={{
-      single_value: text('single value initial value', ''),
-      single_value_forced: select('single value with suggestions initial value', dummyValues, ''),
-      single_value_forced_with_values: select('single value with suggestions initial value and filters', dummyMonth, ''),
-    }}
-    filters={[{
-      property: 'single_value',
-      label: 'Single value filter without values',
-      type: TYPE_SINGLE,
-    }, {
-      property: 'single_value_forced',
-      label: 'Single value with values',
-      type: TYPE_SINGLE,
-      values: ['développeur', 'UX designer', 'Chef de projet'],
-    }, {
-      property: 'single_value_forced_with_values',
-      label: 'Single value filter with values',
-      type: TYPE_SINGLE,
-      values: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ],
-    }]}
-  />
+  <Parent initData={{
+    single_value: '',
+    single_value_forced: 'Developper',
+    single_value_forced_with_values: '3',
+  }}
+  >
+    {(state, setState) => (
+      <Filters
+        translate={translateMockWithDefaultValue}
+        onChange={data => {
+          setState(data);
+          action('Properties update')(data);
+        }}
+        properties={state}
+        filters={[{
+          property: 'single_value',
+          label: 'Single value filter without values',
+          type: TYPE_SINGLE,
+        }, {
+          property: 'single_value_forced',
+          label: 'Single value with values',
+          type: TYPE_SINGLE,
+          values: ['Developper', 'UX designer', 'Project manager'],
+        }, {
+          property: 'single_value_forced_with_values',
+          label: 'Single value filter with values',
+          type: TYPE_SINGLE,
+          // values: object('Select values', dummyMonth),
+          values: dummyMonth,
+        }]}
+      />
+    )}
+  </Parent>
 ));
 
 storiesOf('Components/Filters', module).add('Many', () => (
-  <Filters
-    onChange={onChange}
-    translate={translateMockWithDefaultValue}
-    properties={{
-      many_values: optionsKnob(
-        'Many values with checkboxes',
-        dummyValues,
-        [],
-        { display: 'multi-select' },
-      ),
-      many_values_select_forced: optionsKnob(
-        'Many values forced as select',
-        dummyValues,
-        [],
-        { display: 'multi-select' },
-      ),
-    }}
-    filters={[{
-      property: 'many_values',
-      label: 'Many filter with values',
-      type: TYPE_MANY,
-      values: ['développeur', 'UX designer', 'Chef de projet'],
-    }, {
-      property: 'many_values_select_forced',
-      label: 'Many filter with values forced as select',
-      type: TYPE_MANY,
-      display: 'select',
-      values: ['Développeur', 'UX designer', 'Chef de projet'],
-    }]}
-  />
+  <Parent initData={{
+    many_values: optionsKnob(
+      'Many values with checkboxes',
+      dummyValues,
+      [],
+      { display: 'multi-select' },
+    ),
+    many_values_select_forced: optionsKnob(
+      'Many values forced as select',
+      dummyValues,
+      [],
+      { display: 'multi-select' },
+    ),
+  }}
+  >
+    {(state, setState) => (
+      <Filters
+        onChange={data => {
+          setState(data);
+          action('Properties update')(data);
+        }}
+        translate={translateMockWithDefaultValue}
+        properties={state}
+        filters={[{
+          property: 'many_values',
+          label: 'Many filter with values',
+          type: TYPE_MANY,
+          values: ['Developper', 'UX designer', 'Project manager'],
+        }, {
+          property: 'many_values_select_forced',
+          label: 'Many filter with values forced as select',
+          type: TYPE_MANY,
+          display: 'select',
+          values: ['Developper', 'UX designer', 'Project manager'],
+        }]}
+      />
+    )}
+  </Parent>
 ));
 
 storiesOf('Components/Filters', module).add('Range', () => (
-  <Filters
-    onChange={onChange}
-    properties={{
-      small_range: [
-        number('From (small range)', 30, {}, 'Small range'),
-        number('To (small range)', 70, {}, 'Small range'),
-      ],
-      not_controlable: [1500, 6000],
-      large_range: [
-        number('From (large range)', 15460, {}, 'Large range'),
-        number('To (large range)', 40214, {}, 'Large range'),
-      ],
-    }}
-    filters={[{
-      property: 'small_range',
-      label: 'Small range',
-      type: TYPE_RANGE,
-      // Below, props accepted by https://blueprintjs.com/docs/#core/components/sliders.range-slider
-      labelStepSize: 10,
-      min: number('Min (small range)', 0, {}, 'Small range'),
-      max: number('Max (small range)', 100, {}, 'Small range'),
-    }, {
-      property: 'not_controlable',
-      label: 'Large range with poor precision',
-      type: TYPE_RANGE,
-      max: 10000,
-      // Below, props accepted by https://blueprintjs.com/docs/#core/components/sliders.range-slider
-      stepSize: 500,
-      labelStepSize: 1000,
-    }, {
-      property: 'large_range',
-      label: 'Large range',
-      type: TYPE_RANGE,
-      min: number('Min (large range)', 0, {}, 'Large range'),
-      max: number('Max (large range)', 100000, {}, 'Large range'),
-    }, {
-      property: 'date_range',
-      label: 'Date range',
-      format: 'date',
-      contiguousCalendarMonths: false,
-      allowSingleDayRange: true,
-      type: TYPE_RANGE,
-    }]}
-  />
+  <Parent initData={{
+    small_range: [30, 70],
+    not_controlable: [1500, 6000],
+    large_range: [15460, 40214],
+  }}
+  >
+    {(state, setState) => (
+      <Filters
+        onChange={data => {
+          setState(data);
+          action('Properties update')(data);
+        }}
+        properties={state}
+        filters={[{
+          property: 'small_range',
+          label: 'Small range',
+          type: TYPE_RANGE,
+          // Below, props accepted by https://blueprintjs.com/docs/#core/components/sliders.range-slider
+          labelStepSize: 10,
+          min: number('Min (small range)', 0, {}, 'Small range'),
+          max: number('Max (small range)', 100, {}, 'Small range'),
+        }, {
+          property: 'not_controlable',
+          label: 'Large range with poor precision',
+          type: TYPE_RANGE,
+          max: 10000,
+          // Below, props accepted by https://blueprintjs.com/docs/#core/components/sliders.range-slider
+          stepSize: 500,
+          labelStepSize: 1000,
+        }, {
+          property: 'large_range',
+          label: 'Large range',
+          type: TYPE_RANGE,
+          min: number('Min (large range)', 0, {}, 'Large range'),
+          max: number('Max (large range)', 100000, {}, 'Large range'),
+        }, {
+          property: 'date_range',
+          label: 'Date range',
+          format: 'date',
+          contiguousCalendarMonths: false,
+          allowSingleDayRange: true,
+          type: TYPE_RANGE,
+        }]}
+      />
+    )}
+  </Parent>
 ));
 
 storiesOf('Components/Filters', module).add('Boolean', () => (
-  <Filters
-    onChange={onChange}
-    properties={{
-      switch_value: boolean('Bool initial value'),
-    }}
-    filters={[{
-      property: 'switch_value',
-      label: 'Switch filter',
-      type: TYPE_BOOL,
-    }]}
-  />
+  <Parent initData={{
+    switch_value: boolean('Bool initial value'),
+  }}
+  >
+    {(state, setState) => (
+      <Filters
+        onChange={data => {
+          setState(data);
+          action('Properties update')(data);
+        }}
+        properties={state}
+        filters={[{
+          property: 'switch_value',
+          label: 'Switch filter',
+          type: TYPE_BOOL,
+        }]}
+      />
+    )}
+  </Parent>
 ));
