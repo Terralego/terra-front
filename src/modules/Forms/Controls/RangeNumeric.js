21cd706d0717c491a0d370018e95874332e10552
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-03-31 11:50:07 +0200

    Improve range behaviour

--- src/modules/Forms/Controls/RangeNumeric.js
+++ src/modules/Forms/Controls/RangeNumeric.js
@@ -1,45 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NumericInput, FormGroup, Intent } from '@blueprintjs/core';
+import debounce from 'lodash.debounce';
 import translateMock from '../../../utils/translate';
 import { DEFAULT_MIN, DEFAULT_MAX } from './Range';
 import RangeComponent from './RangeComponent';
+
+const sanitizeProps = ({
+  fetchValues,
+  labelStepSize,
+  min,
+  max,
+  type,
+  onChange,
+  ...props
+}) => props;
 
 /**
  * Two numeric inputs for editing a range.
  */
 class RangeNumeric extends RangeComponent {
+  onChangeDebounced = debounce(range => this.props.onChange(range), 300);
+
   onNumericInputChange = pos => bound => {
-    const { onChange } = this.props;
-    const { range } = this.state;
+    const { min, max } = this.props;
+
     if (Number.isNaN(+bound)) return; // Sanity check that allows typing '-'
-    range[pos] = bound;
-    this.setState({ range });
+
+    this.setState(({ range: prevRange }) => {
+      const nextRange = [...prevRange];
+      nextRange[pos] = bound;
+      return { range: nextRange };
+    });
 
     // Check that the values are within min/max, it will be eventually be
     // clamped, but we need to prevent firing onChange
-    const [lowerB, upperB] = range;
-    if (lowerB <= upperB) {
-      this.setState({ intent: undefined });
-      onChange(range);
-    } else {
-      this.setState({ intent: Intent.DANGER });
+    const { range: [lowerB, upperB] = [] } = this.state;
+
+    if (lowerB < min) {
+      this.setState({ intentLow: Intent.DANGER });
+    }
+
+    if (upperB > max) {
+      this.setState({ intentUp: Intent.DANGER });
+    }
+
+    if (lowerB > upperB) {
+      this.setState({ intentUp: Intent.DANGER, intentLow: Intent.DANGER });
+    }
+
+    if (lowerB <= upperB && lowerB >= min && upperB <= max) {
+      this.setState({ intentLow: undefined, intentUp: undefined });
+      if (lowerB === min && upperB === max) {
+        this.onChangeDebounced(undefined);
+      } else {
+        this.onChangeDebounced([lowerB, upperB]);
+      }
     }
   };
 
   render () {
-    const { onChange, translate, ...props } = this.props;
-    const { intent, range: [lowerBound, upperBound] } = this.state;
+    const { translate, ...props } = this.props;
+    const { intentLow, intentUp, range: [lowerBound, upperBound] } = this.state;
 
     const inputProps = {
       selectAllOnFocus: true,
       clampValueOnBlur: true,
       buttonPosition: 'none',
-      ...props,
-      min: null,
-      max: null,
-      type: undefined, // This resets the type given by Control
-      intent,
+      ...sanitizeProps(props),
     };
 
     return (
@@ -50,6 +78,7 @@ class RangeNumeric extends RangeComponent {
         >
           <NumericInput
             {...inputProps}
+            intent={intentLow}
             onValueChange={this.onNumericInputChange(0)}
             value={lowerBound}
           />
@@ -60,6 +89,7 @@ class RangeNumeric extends RangeComponent {
         >
           <NumericInput
             {...inputProps}
+            intent={intentUp}
             onValueChange={this.onNumericInputChange(1)}
             value={upperBound}
           />
