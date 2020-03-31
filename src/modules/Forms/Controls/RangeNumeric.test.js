96fcabce3ca5e4b76c9f5a104419396aa414d6f1
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-03-31 11:50:07 +0200

    Improve range behaviour

--- src/modules/Forms/Controls/RangeNumeric.test.js
+++ src/modules/Forms/Controls/RangeNumeric.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 import RangeNumeric from './RangeNumeric';
 
+jest.mock('lodash.debounce', () => fn => param => fn(param));
+
 jest.mock('@blueprintjs/core', () => ({
   FormGroup: ({ children }) => <div>{children}</div>,
   NumericInput: () => <p>Numeric input</p>,
@@ -32,24 +34,71 @@ it('should update range numeric if props are changed', () => {
   expect(wrapper.state().range).toEqual([0, 500]);
 });
 
+
+it('should reset range if value are minmaxed', () => {
+  const onChange = jest.fn();
+  const wrapper = mount(<RangeNumeric min={10} max={90} onChange={onChange} />);
+
+  wrapper.find('NumericInput').first().prop('onValueChange')(30);
+  wrapper.find('NumericInput').last().prop('onValueChange')(80);
+  wrapper.update();
+
+  expect(onChange).toHaveBeenLastCalledWith([30, 80]);
+
+
+  wrapper.find('NumericInput').first().prop('onValueChange')(10);
+  wrapper.find('NumericInput').last().prop('onValueChange')(90);
+  wrapper.update();
+
+  expect(onChange).toHaveBeenLastCalledWith(undefined);
+});
+
 it('should handle danger intent', () => {
   const onChange = jest.fn();
   const wrapper = mount(
-    <RangeNumeric max={1000} onChange={onChange} value={[10, 20]} />,
+    <RangeNumeric min={10} max={1000} onChange={onChange} value={[10, 20]} />,
   );
   wrapper.find('NumericInput').first().prop('onValueChange')(30);
   wrapper.update();
   expect(wrapper.find('NumericInput').first().props().intent).toEqual('danger');
+
   expect(onChange).not.toHaveBeenCalled();
+  onChange.mockClear();
+
+  wrapper.find('NumericInput').first().prop('onValueChange')(10);
+  wrapper.update();
+
+  expect(wrapper.find('NumericInput').first().props().intent).toEqual(undefined);
+  expect(onChange).toHaveBeenCalled();
+  onChange.mockClear();
+
+  wrapper.find('NumericInput').first().prop('onValueChange')(8);
+  wrapper.update();
+
+  expect(wrapper.find('NumericInput').first().props().intent).toEqual('danger');
+  expect(onChange).not.toHaveBeenCalled();
+  onChange.mockClear();
+
+  wrapper.find('NumericInput').first().prop('onValueChange')(10);
+  wrapper.update();
+
+  expect(wrapper.find('NumericInput').first().props().intent).toEqual(undefined);
+  expect(onChange).toHaveBeenCalled();
+  onChange.mockClear();
+
+  wrapper.find('NumericInput').last().prop('onValueChange')(1001);
+  wrapper.update();
+
+  expect(wrapper.find('NumericInput').last().props().intent).toEqual('danger');
+  expect(onChange).not.toHaveBeenCalled();
+  onChange.mockClear();
 });
 
 it('should not take consideration of props min or max', () => {
-  const onChange = jest.fn();
   const wrapper = mount(
-    <RangeNumeric max={1000} min={100} onChange={onChange} value={[10, 20]} />,
+    <RangeNumeric max={1000} min={100} value={[10, 20]} />,
   );
-  expect(wrapper.find('NumericInput').first().props().min).toEqual(null);
-  expect(wrapper.find('NumericInput').first().props().max).toEqual(null);
+  expect(wrapper.find('NumericInput').first().props().min).toEqual(undefined);
+  expect(wrapper.find('NumericInput').first().props().max).toEqual(undefined);
   wrapper.find('NumericInput').first().prop('onValueChange')(0);
-  expect(onChange).toHaveBeenCalledWith([0, 20]);
 });
