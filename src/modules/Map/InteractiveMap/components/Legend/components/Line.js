c2935f4472a5e83a555726ccc8d8b6ee94d93621
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-04-02 15:31:24 +0200

    Add line legend

---
+++ src/modules/Map/InteractiveMap/components/Legend/components/Line.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Line = ({ color, size = 16, strokeWidth = 2 }) => (
+  <div className="tf-legend__symbol">
+    <svg
+      viewBox={`0 0 ${size} ${size}`}
+      width={size}
+      height={size}
+    >
+      <line x1="0" y1="0" x2={size} y2={size} style={{ stroke: color, 'stroke-width': strokeWidth }} />
+    </svg>
+  </div>
+);
+
+export default Line;
