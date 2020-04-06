ae820807e7edc8c7361ac9dcb3a2c55be79df6b0
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-04-06 20:49:33 +0200

    Better circle and rect legend

--- src/modules/Map/InteractiveMap/components/Legend/components/Circle.js
+++ src/modules/Map/InteractiveMap/components/Legend/components/Circle.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-export const Circle = ({ color, size }) => (
+export const Circle = ({ color, size = 16, strokeColor = '#111111', strokeWidth = 1 }) => (
   <div className="tf-legend__symbol">
     <svg
-      viewBox="0 0 100 100"
+      viewBox={`0 0 ${size + strokeWidth * 2} ${size + strokeWidth * 2}`}
       width={size}
       height={size}
     >
-      <circle fill={color} cx="50" cy="50" r="50" />
+      <circle
+        fill={color}
+        cx={size / 2 + strokeWidth}
+        cy={size / 2 + strokeWidth}
+        r={size / 2}
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+      />
     </svg>
   </div>
 );
