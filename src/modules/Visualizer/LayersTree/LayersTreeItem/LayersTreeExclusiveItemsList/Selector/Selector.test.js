48817aa887134c0e1139a77bd1c6a12caebbccb7
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-04-07 18:14:24 +0200

    Fix selector when unmounting

--- src/modules/Visualizer/LayersTree/LayersTreeItem/LayersTreeExclusiveItemsList/Selector/Selector.test.js
+++ src/modules/Visualizer/LayersTree/LayersTreeItem/LayersTreeExclusiveItemsList/Selector/Selector.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 import Selector from './Selector';
 
 jest.mock('uuid/v4', () => () => 'uuid');
 
 it('should render', () => {
-  const tree = renderer.create(
+  const { asFragment } = render(
     <Selector
       selectors={[{
         label: 'Foo',
@@ -38,11 +38,11 @@ it('should render', () => {
       }}
     />,
   );
-  expect(tree.toJSON()).toMatchSnapshot();
+  expect(asFragment()).toMatchSnapshot();
 });
 
-it('should render an error message', () => {
-  const tree = renderer.create(
+it('should render even without selector', () => {
+  const { asFragment } = render(
     <Selector
       selectors={[{
         label: 'Foo',
@@ -66,27 +66,58 @@ it('should render an error message', () => {
           bar: 'bar',
         },
       }]}
-      activeLayer={{
+      activeLayer={{}}
+    />,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it('should render an error message', async () => {
+  const { asFragment } = render(
+    <Selector
+      selectors={[{
+        label: 'Foo',
+        name: 'foo',
+        values: [{ label: 'Foo1', value: 'foo' }, { label: 'Bar1', value: 'bar' }],
+      }, {
+        label: 'Bar',
+        name: 'bar',
+        values: [{ label: 'Foo2', value: 'foo' }, { label: 'Bar2', value: 'bar' }],
+      }]}
+      layers={[{
+        label: 'Layer 1',
         selectorKey: {
           foo: 'foo',
+          bar: 'foo',
+        },
+      }, {
+        label: 'Layer 1',
+        selectorKey: {
+          foo: 'foo',
+          bar: 'bar',
+        },
+      }]}
+      activeLayer={{
+        selectorKey: {
+          foo: 'bar', // Layer with this selector don't exists
           bar: 'foo',
         },
       }}
     />,
   );
-  tree.getInstance().setState({ noMatchingLayer: true });
-  expect(tree.toJSON()).toMatchSnapshot();
+
+  expect(asFragment()).toMatchSnapshot();
 });
 
-it('should change layer', () => {
+it('should change layer', async () => {
   const selectors = [{
     label: 'Foo',
     name: 'foo',
-    values: [{ label: 'Foo', value: 'foo' }, { label: 'Bar', value: 'bar' }],
+    values: [{ label: 'Foo1', value: 'foo' }, { label: 'Bar1', value: 'bar' }],
   }, {
     label: 'Bar',
     name: 'bar',
-    values: [{ label: 'Foo', value: 'foo' }, { label: 'Bar', value: 'bar' }],
+    values: [{ label: 'Foo2', value: 'foo' }, { label: 'Bar2', value: 'bar' }, { label: 'Baz2', value: 'baz' }],
   }];
   const layers = [{
     label: 'Layer 1',
@@ -95,27 +126,55 @@ it('should change layer', () => {
       bar: 'foo',
     },
   }, {
-    label: 'Layer 1',
+    label: 'Layer 2',
     selectorKey: {
       foo: 'foo',
       bar: 'bar',
     },
+  }, {
+    label: 'Layer 3',
+    selectorKey: {
+      foo: 'bar',
+      bar: 'foo',
+    },
+  }, {
+    label: 'Layer 4',
+    selectorKey: {
+      foo: 'bar',
+      bar: 'bar',
+    },
   }];
   const onChange = jest.fn();
-  const instance = new Selector({
-    selectors,
-    layers,
-    activeLayer: layers[0],
-    onChange,
-  });
-  instance.setState = jest.fn();
-  instance.onChange('bar')('bar');
-  expect(onChange).toHaveBeenCalledWith(1);
-  expect(instance.setState).toHaveBeenCalledWith({ noMatchingLayer: false });
-  onChange.mockClear();
-  instance.setState.mockClear();
 
-  instance.onChange('bar')('lorem');
-  expect(onChange).not.toHaveBeenCalledWith(1);
-  expect(instance.setState).toHaveBeenCalledWith({ noMatchingLayer: true });
+  const { asFragment } = render(
+    <Selector
+      selectors={selectors}
+      layers={layers}
+      activeLayer={layers[0]}
+      onChange={onChange}
+    />,
+  );
+
+  // First selector
+  fireEvent.click(screen.getByText('Foo1'));
+  fireEvent.click(screen.getByText('Bar1'));
+
+  expect(onChange).toHaveBeenLastCalledWith(2);
+
+  onChange.mockClear();
+
+  // Second selector
+  fireEvent.click(screen.getByText('Foo2'));
+
+  fireEvent.click(screen.getByText('Bar2'));
+
+  expect(onChange).toHaveBeenLastCalledWith(1);
+
+  onChange.mockClear();
+
+  fireEvent.click(screen.getByText('Baz2')); // Selector with no layer
+
+  expect(onChange).not.toHaveBeenCalled();
+
+  expect(asFragment()).toMatchSnapshot();
 });
