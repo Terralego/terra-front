6b586833eaae65eef05462b5a2d9fc3ccae244fe
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-05-11 18:07:11 +0200

    Improve MainMenu component

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
+  Classes,
   Navbar,
   NavbarGroup,
 } from '@blueprintjs/core';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import NavBarItemDesktop from './NavBarItemDesktop';
 import NavBarItemTablet from './NavBarItemTablet';
@@ -11,45 +13,57 @@ import NavBarItemTablet from './NavBarItemTablet';
 import withDeviceSize from '../hoc/withDeviceSize';
 import { connectAuthProvider } from '../modules/Auth';
 
-
-/* eslint-disable react/no-array-index-key */
 export const MainMenu = ({
+  className,
   isMobileSized,
+  isPhoneSized,
   navItems,
-}) => (
-  <Navbar className="navBar bp3-dark">
-    {
-          navItems.map((group, index) => (
-            <NavbarGroup key={index} className="navBar__group">
-              {group.map(({ component: Component = isMobileSized
-                ? NavBarItemTablet : NavBarItemDesktop, ...item }) => (
-                  <Component
-                    key={item.label}
-                    icon={item.icon}
-                    {...item}
-                  />
-              ))}
-            </NavbarGroup>
-          ))
-        }
-  </Navbar>
-);
+  ...props
+}) => {
+  if (!navItems.length) {
+    return null;
+  }
+
+  const defaultComponent = isMobileSized ? NavBarItemTablet : NavBarItemDesktop;
+
+  return (
+    <Navbar className={classnames(['navBar', Classes.DARK, className])} {...props}>
+      {navItems.map((group, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <NavbarGroup key={index} className="navBar__group">
+          {group.map(({ component: Component = defaultComponent, ...item }) => (
+            <Component
+              key={item.id}
+              {...item}
+            />
+          ))}
+        </NavbarGroup>
+      ))}
+    </Navbar>
+  );
+};
 
 MainMenu.propTypes = {
+  className: PropTypes.string,
   isMobileSized: PropTypes.bool,
+  isPhoneSized: PropTypes.bool,
   navItems: PropTypes.arrayOf(
     PropTypes.arrayOf(
       PropTypes.shape({
+        component: PropTypes.func,
+        href: PropTypes.string,
+        icon: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
         id: PropTypes.string,
         label: PropTypes.string,
-        icon: PropTypes.string,
       }),
     ),
   ),
 };
 
 MainMenu.defaultProps = {
+  className: '',
   isMobileSized: false,
+  isPhoneSized: false,
   navItems: [],
 };
 
