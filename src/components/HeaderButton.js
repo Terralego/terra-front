c4a55d7378cf2d2256c7547cda0b8b69d616074d
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-05-11 18:07:45 +0200

    Improve HeaderButton to also accept Keyword and Component as Icon

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-import { Button } from '@blueprintjs/core';
+import { Classes, Button } from '@blueprintjs/core';
 
-export const HeaderButton = ({ id, icon, alt }) => (
-  <Button
-    className={`header-button ${id}`}
-    minimal
-    id={id}
-  >
-    {icon
-      ? <img src={icon} alt={alt} />
-      : <span>{alt}</span>}
-  </Button>
-);
+const getIcon = (icon, alt) => {
+  if (!icon) {
+    return null;
+  }
+  const isPathIcon = typeof icon === 'string' && icon.includes('.');
+  return !isPathIcon
+    ? icon
+    : (
+      <span className={Classes.ICON}>
+        <img src={icon} alt={alt} />
+      </span>
+    );
+};
+
+
+export const HeaderButton = ({ alt, className, icon, ...props }) => {
+  const formattedIcon = getIcon(icon, alt);
+
+  return (
+    <Button
+      className={classnames(['header-button', className])}
+      icon={formattedIcon}
+      minimal
+      text={!formattedIcon && alt}
+      {...props}
+    />
+  );
+};
 
 HeaderButton.propTypes = {
-  id: PropTypes.string.isRequired,
-  icon: PropTypes.string,
   alt: PropTypes.string,
+  className: PropTypes.string,
+  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
 };
 
 HeaderButton.defaultProps = {
-  icon: '',
   alt: '',
+  className: '',
+  icon: undefined,
 };
 
 export default HeaderButton;
