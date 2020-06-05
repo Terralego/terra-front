e2a66d7a72a2c744f60c8577d63bed1c50a405d4
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-05 15:40:55 +0200

    Clean isPhoneSized prop in MainMenuItem

--- src/components/MainMenuItem.js
+++ src/components/MainMenuItem.js
@@ -5,7 +5,7 @@ import withDeviceSize from '../hoc/withDeviceSize';
 import NavBarItemDesktop from './NavBarItemDesktop';
 import NavBarItemTablet from './NavBarItemTablet';
 
-const MainMenuItem = ({ component, isMobileSized, id, ...props }) => {
+const MainMenuItem = ({ component, isMobileSized, isPhoneSized, id, ...props }) => {
   const defaultComponent = isMobileSized ? NavBarItemTablet : NavBarItemDesktop;
   const Component = component || defaultComponent;
 
@@ -21,12 +21,14 @@ const MainMenuItem = ({ component, isMobileSized, id, ...props }) => {
 MainMenuItem.propTypes = {
   component: PropTypes.func,
   isMobileSized: PropTypes.bool,
+  isPhoneSized: PropTypes.bool,
   id: PropTypes.string,
 };
 
 MainMenuItem.defaultProps = {
   component: null,
   isMobileSized: false,
+  isPhoneSized: false,
   id: undefined,
 };
 
