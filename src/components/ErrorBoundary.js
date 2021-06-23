be1a6c60707d3569d8bd74eadbc0ae3dbcc961b1
CFarcy <cedric.farcy@makina-corpus.com>
2021-06-23 14:10:38 +0200

    Update src/components/ErrorBoundary.js
    Co-authored-by: Flo <florian.sommariva@makina-corpus.com>

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 export default class ErrorBoundary extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+  state = { hasError: false };
 
   static getDerivedStateFromError () {
     return { hasError: true };
