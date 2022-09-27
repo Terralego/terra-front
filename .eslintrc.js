7423eea6978db209178a269b088983a169e44d93
Cédric Farcy <cedric.farcy@makina-corpus.com>
2022-09-27 17:10:24 +0200

    Update esling conf to match current codebase style

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,11 @@ module.exports = {
   rules: {
     'import/no-cycle':                   [off],
     'operator-linebreak':                [off],
+    'no-multiple-empty-lines':           [off],
+    'react/jsx-curly-brace-presence':    [off],
+    'react/forbid-prop-types':           [off],
+    'react/require-default-props':       [off],
+    'react/jsx-props-no-multi-spaces':   [off],
     'react/destructuring-assignment':    [error],
     'react/no-this-in-sfc':              [error],
     'react/no-access-state-in-setstate': [error],
